2a7b0e80094f6de0c3efd270975bb6576885f362
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2021-01-27 23:42:03 +0100

    GET route done. returns a status code of 200 along with the data

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const app = express()
 const port = 3000
 app.use(express.json())
 
+app.get('/', async(req, res) => {
+  return res.status(200).json({
+    message: "My Rule-Validation API",
+    status: "success",
+    data: {
+      name: "Tobiloba Owolabi",
+      github: "@owolabiezekiel",
+      email: "owo.ezekiel@gmail.com",
+      mobile: "08106723916",
+      twitter: "@Toby_Ezekiel"
+    }
+  })
+})
+
 app.listen(port, () => {
-    console.log(`Flutterwave technical assessment app listening at http://localhost:${port}`);
+  console.log(`Flutterwave technical assessment app listening at http://localhost:${port}`);
 })
